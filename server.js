627818523c1d8c980c243b774bea439a36ff17a6
carlos valenzuela <carlos@aureolab.cl>
2022-05-01 20:21:24 -0400

    finish

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js
 // where your node app starts
-
+require('dotenv').config()
 // init project
 var express = require('express');
 var app = express();
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,10 +21,33 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 
+app.get("/api", function (req, res) {
+  let date = new Date()
+  let unix = Math.round(date.getTime() / 1000)
+  let utc = date.toUTCString()
+
+  if (!unix) return res.json({ error: "Invalid Date" })
+  res.json({ unix, utc });
+});
+
+
+app.get("/api/:date", (req, res) => {
+
+  let regex = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/g
+
+  let parseDate = regex.test(req.params.date) ? req.params.date : parseInt(req.params.date)
+  let date = new Date(parseDate)
+
+  let unix = Math.round(date.getTime() / 1000)
+  let utc = date.toUTCString()
+
+  if (!unix) return res.json({ error: "Invalid Date" })
+  res.json({ unix, utc });
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
